353902c48ea4b084dc6c486bed98889c8949cbd6
Rafael Matuoka Chiocchetti <rmatuoka@gmail.com>
2012-12-08 13:55:17 -0200

    Up

---
+++ config/initializers/setup_mail.rb
@@ -0,0 +1,13 @@
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :address => "mail.farmaciaecovita.com.br",
+  :port => 587,
+  :domain => "farmaciaecovita.com.br",
+  :user_name => "vendas@farmaciaecovita.com.br",
+  :password => "vendas@@@",
+  :authentication => :login,
+  :enable_starttls_auto => false  
+}
+ActionMailer::Base.default_url_options[:host] = "localhost:3000"
+require 'development_mail_interceptor'
+ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
